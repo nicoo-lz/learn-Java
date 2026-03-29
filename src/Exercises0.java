59f1f8f3e02eeab15d9b882513eee3c2b877cf35
Nico <nicohuamani6470@gmail.com>
2026-03-29 15:02:18 -0300

    finish exercises0

--- src/Exercises0.java
+++ src/Exercises0.java
@@ -3,24 +3,53 @@ import java.util.Scanner;
 
 public class Exercises0 {
     public static void main(String[] args) {
-        // EJ 1) Imprime un mensaje que diga tu nombre en lugar de "¡Hola Mundo!".
+        /* EJ 1) Imprime un mensaje que diga tu nombre en lugar de "¡Hola Mundo!" */
 
         // instancio la clase Scanner
         Scanner teclado = new Scanner(System.in);
+
+        // prompteo al usuario por su nombre
         System.out.print("¿Cómo te llamas?: ");
+
         // leo el input del usuario, que sera String
         String name = teclado.nextLine();
-        // saludo con un format specifier para el String
+
+        // saludo al usuario con un format specifier para el tipo String
         System.out.printf("¡Hola %s!", name);
 
-        // EJ 2) Imprime dos líneas: "Hola" y luego "Mundo" con un solo println.
-        // EJ 3) Añade un comentario sobre lo que hace cada línea del programa.
-        // EJ 4) Crea un comentario en varias líneas.
-        // EJ 5) Imprime tu edad, tu color favorito y tu ciudad.
-        // EJ 6) Explora los diferentes System.XXX.println(); más allá de "out".
-        // EJ 7) Utiliza varios println para imprimir una frase.
-        // EJ 8) Imprime un diseño ASCII (por ejemplo, una cara feliz usando símbolos).
-        // EJ 9) Intenta ejecutar el programa sin el metodo main y observa el error.
-        // EJ 10) Intenta cambiar el nombre del archivo a uno diferente del de la clase y compílalo. ¿Qué pasa?
+        // cerramos el objeto
+        teclado.close();
+
+        /* EJ 2) Imprime dos líneas: "Hola" y luego "Mundo" con un solo println. */
+
+        // uso el escape sequence \n para insertar un salto de linea
+        System.out.println("Hola\nMundo");
+
+        /* EJ 3) Imprime tu edad, tu color favorito y tu ciudad. */
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("¿Cual es tu edad?: ");
+
+        /*
+            preferimos usar nextLine() al leer ints para no tener problemas con el \n
+            si luego queremos leer Strings por terminal. Entonces, leemos el int como String
+            y luego lo convertimos a int con parseInt()
+        */
+        int age = Integer.parseInt(keyboard.nextLine());
+
+        System.out.print("¿Cual es tu color fav?: ");
+        String colour = keyboard.nextLine();
+        System.out.print("¿Donde vivis?: ");
+        String city = keyboard.nextLine();
+        keyboard.close();
+        System.out.printf("Edad: %d, Color: %s, Ciudad: %s\n", age, colour, city);
+
+        // EJ 4) Explora los diferentes System.XXX.println(); más allá de "out".
+
+        System.out.println("Error grave LOL");
+
+        // muestra el mismo mensaje pero en rojo
+        System.err.println("Error grave LOL");
+
     }
 }
